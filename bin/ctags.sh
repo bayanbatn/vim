a59de7eb3f34f897a37947b01f66450b9b1d33bf
Naran Bayanbat <naranbaatar.bayanbat@opendoor.com>
2022-02-13 15:36:21 -0800

    go tags update

--- bin/ctags.sh
+++ bin/ctags.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rg --files --follow | ctags -R --links=no -L -
+rg --type-add 'tests:*test*.py' --type-not tests --type-add 'note:*.ipynb' --type-not note --files --follow | ctags -R --links=no -L -
